70b2e1e19f17be32a69cd5229906ba13474151e9
Philip Hoover <philhoov90@gmail.com>
2018-10-10 11:48:44 -0600

    created CRUDable array of objects

---
+++ components.js
@@ -0,0 +1,16 @@
+const components = [
+    {part: "Motherboard", description: "The core of the system. It really is the PC; everything else is connected to it, and it controls everything in the system."},
+    {part: "Processor", description: "Often thought of as the 'engine' of the computer. It's also called the CPU (central processing unit)."},
+    {part: "Memory/RAM", description: "Random Access Memory. This is the primary memory, which holds all the programs and data the processor is using at a given time."},
+    {part: "Case/chassis", description: "Houses the motherboard, power supply, disk drives, adapter cards, and any other physical components in the system."},
+    {part: "Power supply", description: "Feeds electrical power to every single part in the PC."},
+    {part: "Hard disk", description: "The primary archival storage memory for the system."},
+    {part: "Disc drive", description: "High-capacity, removable media, optical drives that can read CDs, DVDs, etc."},
+    {part: "Keyboard", description: "Primary input device on a PC that is used by a human to communicate with and control a system."},
+    {part: "Mouse", description: "Another input device used to select objects from and interact with the GUI."},
+    {part: "Video card", description: "Controls the information you see on the monitor."},
+    {part: "Monitor", description: "Screen that displays information from the CPU."},
+    {part: "Sound card", description: "Interface that enables the PC to generate complex sounds."},
+    {part: "Speakers", description: "Output device(s) for sounds."},
+    {part: "Modem", description: "Device for connecting to the internet."}
+]
