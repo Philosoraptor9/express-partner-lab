db625e420e3d1d52c198417852a509f2588cf0eb
Philip Hoover <philhoov90@gmail.com>
2018-10-10 14:58:20 -0600

    edit page and put route added

--- server.js
+++ server.js
@@ -23,6 +23,21 @@ Components.splice(req.params.id, 1);
 res.redirect('/components')
 })
 
+// Edit route that populates the forms with data from server, link from index page
+app.get('/components/:id/edit', (req, res) => {
+    const data = {components: Components[req.params.id], id: req.params.id}
+    res.render('edit.ejs', data)
+})
+
+
+// Put route that will change the element of your objects array, as specified by 
+// the index in the URL, to req.body
+app.put('/components/:id', (req, res) => {
+    Components[req.params.id] = req.body;
+    res.redirect('/components')
+})
+
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
